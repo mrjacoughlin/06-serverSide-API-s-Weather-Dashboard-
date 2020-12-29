1ed1c42e22ae329eef5be4777aec942f0ed58486
James Coughlin <mrjacoughlin@gmail.com>
2020-12-29 14:58:51 -0500

    adjusted main page by removing weather list

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-// $(document).ready(function () {
-// $("#search-button").on("click", function () {
-// var searchVal = $("#search-value").val()
-
-var apiKey = "b0c053302397a21466e0caac313fe730";
-var queryURL =
-  "http://api.openweathermap.org/data/2.5/weather?q=london&appid=b0c053302397a21466e0caac313fe730";
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (response) {
-  console.log(queryURL);
-  console.log(response);
+$(document).ready(function () {
+  $("#search-button-city").on("click", function () {
+    var searchTerm = $("#search-term").val();
+    $("#search-term").val("");
+    searchWeather(searchTerm);
+  });
+  function searchWeather(searchTerm) {
+    $.ajax({
+      type: "GET",
+      url:
+        "http://api.openweathermap.org/data/2.5/weather?q=" +
+        searchTerm +
+        "&appid=edd897a328ccf6ebff12cd58fd111ba2&",
+      dataType: "json",
+    });
+  }
 });
